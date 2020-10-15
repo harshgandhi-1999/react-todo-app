d58d96f69e6878cf1c2dcc5f48a289e66084a759
harsh gandhi <harshgandhi043@gmail.com>
2020-10-15 22:26:51 +0530

    removed useless files

--- todo-app-frontend/src/utils/axiosInstance.js
+++ todo-app-frontend/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 const axiosInstance = axios.create();
 
-axiosInstance.defaults.baseURL = "http://localhost:8000/api/todo-app";
+axiosInstance.defaults.baseURL =
+  "https://heroku-todo-app-backend.herokuapp.com/api/todo-app";
 axiosInstance.defaults.headers.common["Content-Type"] = "application/json";
 axiosInstance.defaults.headers.common["Accept"] = "application/json";
 
