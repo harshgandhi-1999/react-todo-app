dc40e58d46e42e3afee67bb5e8c39ed4a4791f1d
harsh gandhi <harshgandhi043@gmail.com>
2020-10-16 02:22:21 +0530

    added gitignore files

--- todo-app-frontend/src/utils/axiosInstance.js
+++ todo-app-frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL =
-  "https://heroku-todo-app-backend.herokuapp.com/api/todo-app";
+  "https://thetodolistbackend.herokuapp.com/api/todo-app";
 axiosInstance.defaults.headers.common["Content-Type"] = "application/json";
 axiosInstance.defaults.headers.common["Accept"] = "application/json";
 
